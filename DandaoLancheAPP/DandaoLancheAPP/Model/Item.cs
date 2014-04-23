bb2b994a40f42326575e0508febf00f738c74ce7
Genilson Ferreira <gr.ferreira@live.com>
2014-04-23 02:01:15 -0700

    manter pedido

---
+++ DandaoLancheAPP/DandaoLancheAPP/Model/Item.cs
@@ -0,0 +1,32 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DandaoLancheAPP.Model
+{
+    public sealed class Item
+    {
+        [PrimaryKey, AutoIncrement]
+        public int iditem { get; set; }
+        public int idpedidoitem { get; set; }
+
+        public string nomeproduto { get; set; }
+
+        public string observacao { get; set; }
+
+        public double preco { get; set; }
+
+        public int quantidade { get; set; }
+
+        public double total { get; set; }
+
+
+        public override string ToString()
+        {
+            return idpedidoitem + ":" + nomeproduto + ":" + observacao + ":" + quantidade + ":" + preco + total + ":";
+        }
+    }
+}
